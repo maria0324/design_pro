b08104530119148d5bea5d83317345fc3d2c76d4
79234 <marusya0323@gmail.com>
2023-11-06 19:49:56 +0700

    настройка шаблона регистрации

--- design_pro/design_pro/views.py
+++ design_pro/design_pro/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
+from django.shortcuts import render
+from django.contrib.auth.views import LoginView
+from django.urls import reverse_lazy
+from django.views.generic.list import ListView
+from django.views.generic import CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LogoutView
+
+from .models import Request
+from django.utils import timezone
 
 def index(request):
    return render(request, 'main/index.html')
+
+class BBLoginView(LoginView):
+   template_name = 'main/login.html'
