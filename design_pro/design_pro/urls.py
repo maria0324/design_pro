c0e3232eecfbe7af835ada3037df7c259c0a611a
79234 <marusya0323@gmail.com>
2023-11-07 10:11:42 +0700

    создание формы

--- design_pro/design_pro/urls.py
+++ design_pro/design_pro/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
 from .views import index
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+from .views import *
 
 app_name = 'designpro'
 
 urlpatterns = [
     path('', index, name='index'),
+    path('register/', RegistrateUser.as_view(), name='registration'),
+    path('login/', BBLoginView.as_view(), name='login'),
+    path('logout/', BBLogoutView.as_view(), name='logout'),
+
 ]
--- design_pro/design_pro/views.py
+++ design_pro/design_pro/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render
-from django.shortcuts import render
 from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy
 from django.views.generic.list import ListView
 from django.views.generic import CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LogoutView
-
+from .forms import UserRegisterForm
 from .models import Request
 from django.utils import timezone
 
